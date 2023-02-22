c6e4710e3e95ac1b4e13d831fdb381192007dcd5
fdariia <fdariya3005@gmail.com>
2023-02-22 20:08:55 +0200

    Create FriendList.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import css from 'components/FriendList/FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <li className={css.item} key={friend.id}>
+          <span className={`${css.status} ${css[friend.isOnline]}`}>
+            {friend.isOnline}
+          </span>
+          <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
